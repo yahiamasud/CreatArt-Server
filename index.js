f8a51df09f28ae68fa50dad0abcb1f82a3a6727b
yahia masud <yahiamasud974@gmail.com>
2023-06-09 23:53:21 +0600

    jwt add

--- index.js
+++ index.js
@@ -3,10 +3,28 @@ const app = express()
 const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const cors = require("cors")
 require("dotenv").config()
+const jwt = require('jsonwebtoken');
 const port = process.env.PORT || 5000
 
 app.use(cors());
 app.use(express.json());
+
+// const verifyJWT = (req, res, next) => {
+//   const authorization = req.headers.authorization;
+//   if (!authorization) {
+//     return res.status(401).send({ error: true, message: 'unauthorized access' });
+//   }
+//   // bearer token
+//   const token = authorization.split(' ')[1];
+
+//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+//     if (err) {
+//       return res.status(401).send({ error: true, message: 'unauthorized access' })
+//     }
+//     req.decoded = decoded;
+//     next();
+//   })
+// }
 
 //  this is the link 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylfmjda.mongodb.net/?retryWrites=true&w=majority`;
@@ -29,7 +47,12 @@ async function run() {
     const classCollection = client.db("CreactArtSchool").collection("class");
     const UserCollection = client.db("CreactArtSchool").collection("user");
 
-
+  //  this is the token jwt
+    app.post('/jwt', (req, res) => {
+      const user = req.body;
+      const token = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET, { expiresIn: '1h' })
+      res.send({ token })
+    })
 
     // this is the class add
     app.post('/class', async (req, res) => {
@@ -38,12 +61,14 @@ async function run() {
       res.send(result);
   })
 
-    // this is the user data save
-
+   
     app.get('/users', async (req, res)=>{
-      const result = await UserCollection.find().toArray();
-      res.send(result)
-    })
+      const items = UserCollection.find();
+      const result = await items.toArray();
+      res.send(result);
+    });
+
+   // this is the user data save
 
     app.post('/users', async(req, res)=>{
       const user= req.body;
