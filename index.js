afcccb7ab84f582fd1dede63769fc5f236055ff3
yahia masud <yahiamasud974@gmail.com>
2023-06-10 13:23:36 +0600

    add the user

--- index.js
+++ index.js
@@ -9,22 +9,22 @@ const port = process.env.PORT || 5000
 app.use(cors());
 app.use(express.json());
 
-// const verifyJWT = (req, res, next) => {
-//   const authorization = req.headers.authorization;
-//   if (!authorization) {
-//     return res.status(401).send({ error: true, message: 'unauthorized access' });
-//   }
-//   // bearer token
-//   const token = authorization.split(' ')[1];
+const verifyJWT = (req, res, next) => {
+  const authorization = req.headers.authorization;
+  if (!authorization) {
+    return res.status(401).send({ error: true, message: 'unauthorized access' });
+  }
+  // bearer token
+  const token = authorization.split(' ')[1];
 
-//   jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
-//     if (err) {
-//       return res.status(401).send({ error: true, message: 'unauthorized access' })
-//     }
-//     req.decoded = decoded;
-//     next();
-//   })
-// }
+  jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, decoded) => {
+    if (err) {
+      return res.status(401).send({ error: true, message: 'unauthorized access' })
+    }
+    req.decoded = decoded;
+    next();
+  })
+}
 
 //  this is the link 
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.ylfmjda.mongodb.net/?retryWrites=true&w=majority`;
@@ -80,7 +80,14 @@ async function run() {
       const result = await UserCollection.insertOne(user)
       res.send(result);
     })
-
+     
+    app.get('/users/admin/:email',async(req,res)=>{
+      const email = req.params.email;
+      const query = {email: email}
+      const user = await UserCollection.findOne(query)
+      const result={admin: user?.role ==="admin"}
+      res.send(result)
+    })
     // admin kora
     app.patch('/users/admin/:id', async(req, res)=>{
       const id= req.params.id;
